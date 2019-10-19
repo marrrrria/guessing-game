fac98268418e4ae0053d15d4ffe6b01ae0e6ad28
Masha <valzyr48@gmail.com>
2019-10-19 17:18:27 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    
+    }
 
     setRange(min, max) {
-
+        this.number = Math.ceil((max - min) / 2) + min;
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.number;
     }
 
     lower() {
-
+        this.max = this.number;
+        this.setRange(this.min, this.max);
     }
 
     greater() {
-
+        this.min = this.number;
+        this.setRange(this.min, this.max);
     }
 }
 
